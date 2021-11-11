49b497032c91deeebb232acd815bd93f01f537fc
vimariano <vincentmariano@hotmail.com>
2021-11-11 14:56:20 -0800

    added array for enemy robots and for loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,28 +1,29 @@
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
 // You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
   window.alert("Welcome to Robot Gladiators!");
   var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+
   if (promptFight === "FIGHT" || promptFight === "fight"){
-      
-  
   enemyHealth = enemyHealth - playerAttack;
-  console.log(enemyName + " health is now " + enemyHealth);
+  console.log(enemyNames[i] + " health is now " + enemyHealth);
   if(enemyHealth <= 0){
-      window.alert(enemyName + " has died");
+      window.alert(enemyNames[i] + " has died");
   }
   else {
-      window.alert(enemyName + " still has " + enemyHealth + " health left.")
+      window.alert(enemyNames[i] + " still has " + enemyHealth + " health left.")
   }
   playerHealth = playerHealth - enemyAttack;
   console.log( playerName + " health is now " + playerHealth)
@@ -53,4 +54,9 @@ var fight = function() {
 
 };
 
-fight();
+// index of last element in array
+// console.log(enemyNames.length - 1)
+
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
